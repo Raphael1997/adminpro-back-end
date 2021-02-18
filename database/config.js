6e88eed8c2272ef6057b6fd36577005b93a154d5
Hozanno Raphael <hozanno11@gmail.com>
2021-02-18 21:32:23 +0100

    test prodducion error

--- database/config.js
+++ database/config.js
@@ -18,7 +18,7 @@ const dbConnection = async () => {
         console.log("DB online");
     } catch (error) {
         console.log(error);
-        throw new Error("Error al iniciar la base de datos")
+        throw new Error(error)
     }
 }
 
